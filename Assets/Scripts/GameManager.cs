574f730bacee3f76404cee4ce9333e0432528365
Kalm004 <kalm004@gmail.com>
2016-08-29 13:35:01 +0100

    Aumento de dificultad.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,7 @@
     public static int score = 0;
     public static int lifes = 3;
     public const int maxLifes = 3;
-    public const float minSpeed = 5;
+    public const float minSpeed = 7;
     public const float maxSpeed = 15;
 
     public static float currentSpeed;
